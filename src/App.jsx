d0c0292e20186f367325223d30640edc1361a311
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-02-12 22:06:57 -0600

    Add navbar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-   <h1>Wassup</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
   );
 }
 
